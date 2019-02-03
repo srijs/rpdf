0653ab58a12e366309437e8c6dde01bf78359f4a
Sam Rijs <srijs@airpost.net>
2019-02-03 18:45:28 +1100

    avoid utf8 parsing for glyph name lookup

--- lib/glyph_names/build.rs
+++ lib/glyph_names/build.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 use std::env;
+use std::fmt;
 use std::fs::File;
+use std::hash::{Hash, Hasher};
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
 
@@ -28,13 +30,37 @@ fn main() {
     let mut file = BufWriter::new(File::create(&path).unwrap());
     write!(
         &mut file,
-        "static GLYPH_MAP: phf::Map<&'static str, char> = "
+        "static GLYPH_MAP: phf::Map<&'static [u8], char> = "
     )
     .unwrap();
     let mut map_builder = phf_codegen::Map::new();
     for (key, value) in map {
-        map_builder.entry(key, &value);
+        map_builder.entry(GlyphKey(key), &value);
     }
     map_builder.build(&mut file).unwrap();
     write!(&mut file, ";\n").unwrap();
+}
+
+#[derive(PartialEq, Eq)]
+struct GlyphKey(String);
+
+impl Hash for GlyphKey {
+    fn hash<H>(&self, h: &mut H)
+    where
+        H: Hasher,
+    {
+        self.0.as_bytes().hash(h)
+    }
+}
+
+impl phf_shared::PhfHash for GlyphKey {
+    fn phf_hash<H: Hasher>(&self, state: &mut H) {
+        self.0.as_bytes().phf_hash(state)
+    }
+}
+
+impl fmt::Debug for GlyphKey {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "&{:?}", self.0.as_bytes())
+    }
 }
--- lib/glyph_names/src/lib.rs
+++ lib/glyph_names/src/lib.rs
@@ -3,14 +3,10 @@ const UNICODE_REPLACEMENT_CHAR: char = '\u{FFFD}';
 include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 
 pub fn glyph_name_to_char(name: &[u8]) -> char {
-    if let Ok(name_str) = std::str::from_utf8(name) {
-        GLYPH_MAP
-            .get(name_str)
-            .cloned()
-            .unwrap_or(UNICODE_REPLACEMENT_CHAR)
-    } else {
-        UNICODE_REPLACEMENT_CHAR
-    }
+    GLYPH_MAP
+        .get(name)
+        .cloned()
+        .unwrap_or(UNICODE_REPLACEMENT_CHAR)
 }
 
 #[cfg(test)]
